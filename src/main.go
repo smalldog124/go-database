f18af0278fc74f2c6125cd22ef60313a3c84ece6
smalldog124 <panumars@gmail.com>
2018-07-09 15:01:43 +0700

    [Added]: query ข้อมูลในตาราง user

--- src/main.go
+++ src/main.go
@@ -7,11 +7,52 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Usersdata struct {
+	ID              int
+	CitizenID       string
+	Fristname       string
+	Lastname        string
+	BirthYear       int
+	FristnameFather string
+	LastnameFather  string
+	FristnameMother string
+	LastnameMother  string
+	SoldierID       int
+	AddressID       int
+}
+
 func main() {
-	_, err := sql.Open("mysql", "root:tcp(127.0.0.1:3306)/testsck")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
 	if err != nil {
 		fmt.Println("connect fail")
 	} else {
 		fmt.Println("connect success")
+	}
+
+	defer db.Close()
+
+	result, _ := db.Query("SELECT * FROM testsck.user")
+
+	for result.Next() {
+		var user Usersdata
+		err := result.Scan(
+			&user.ID,
+			&user.CitizenID,
+			&user.Fristname,
+			&user.Lastname,
+			&user.BirthYear,
+			&user.FristnameFather,
+			&user.LastnameFather,
+			&user.FristnameMother,
+			&user.LastnameMother,
+			&user.SoldierID,
+			&user.AddressID,
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(user)
 	}
 }
